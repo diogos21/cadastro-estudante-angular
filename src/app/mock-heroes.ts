ce55e848964080f9ed82e66298b1b862db02486e
Pichau <diogo.sucupira01@outlook.com>
2021-05-04 21:33:08 -0300

    :package:Part 2

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Diogo' },
+  { id: 2, name: 'Matheus Fukuda' },
+  { id: 3, name: 'Luiz Miguel' },
+  { id: 4, name: 'Kadu Lisboa' },
+  { id: 5, name: 'Eliabe Ramos' },
+  
+];
